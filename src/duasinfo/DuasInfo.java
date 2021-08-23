9fe6780ba94c4e6cbb969a272986fcdc75e1c8cc
kaykydlima <kayky140703@gmail.com>
2021-08-22 21:41:46 -0300

    duasInfo

---
+++ src/duasinfo/DuasInfo.java
@@ -0,0 +1,17 @@
+
+package duasinfo;
+
+public class DuasInfo {
+
+    public static void main(String[] args) {
+        int x,y;
+        x = 90;
+        y = 30;
+        System.out.println("x = " + x + " y = " + y);
+        //mudando valor
+        x = 20;
+        y = 15;
+        System.out.println("x = " + x + " y = " + y);
+    }
+    
+}
